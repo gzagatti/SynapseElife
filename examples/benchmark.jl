2e2e0ef2d5654d49027208e79d7681b424e9cb1b
gzagatti <gzagatti@users.noreply.github.com>
2023-04-25 16:16:19 +0800

    adds benchmark script

---
+++ examples/benchmark.jl
@@ -0,0 +1,104 @@
+using Synapse
+using PiecewiseDeterministicMarkovProcesses, JumpProcesses, Plots
+using BenchmarkTools
+
+root = dirname(@__DIR__)
+assets = "$(root)/assets"
+
+# parameters
+p_synapse = SynapseParams(t_end = 1000.);
+glu = 0.0;
+events_sorted_times = [500.];
+is_pre_or_post_event = [true];
+events_bap = events_sorted_times[is_pre_or_post_event .== false];
+bap_by_epsp = Float64[];
+t1 = 0.;
+t2 = 500.;
+nu = buildTransitionMatrix();
+
+# initial conditions
+xc0 = initial_conditions_continuous_temp(p_synapse);
+xd0 = initial_conditions_discrete(p_synapse);
+
+# algorithms to benchmark
+solver = AutoTsit5(Rosenbrock23());
+algorithms = [
+	(
+		label = "PDMP",
+		agg = nothing,
+		solver = (CHV(solver), CHV(solver)),
+		save_positions = (false, true), # necessary to save at regular intervals
+		saveat = [],
+	),
+	(
+		label = "Coevolve",
+		agg = CoevolveSynced(),
+		solver = (solver, solver),
+		save_positions = (false, false),
+		saveat = 0.05, # similar rate as the PDMP problem
+	),
+];
+
+# initial run for pre-compilation purposes
+results = []
+
+for algo in algorithms
+	push!(results, evolveSynapse(
+		xc0,
+		xd0,
+		p_synapse,
+		events_sorted_times,
+		is_pre_or_post_event,
+		bap_by_epsp,
+		[true],
+		nu,
+		algo.solver,
+		algo.agg;
+		save_positions = algo.save_positions,
+		saveat = algo.saveat,
+		save_everystep = false,
+	))
+end
+
+# plot results to check they are correct
+fig = plot(xlabel="Voltage", ylabel="Time");
+for (i, algo) in enumerate(algorithms)
+	res = results[i]
+	plot!(res.t, res.Vsp, label=algo.label)
+end
+title!("Vsp")
+
+fig = plot(xlabel="N", ylabel="Time");
+for (i, algo) in enumerate(algorithms)
+	res = results[i]
+	plot!(res.t, res.XD[1, :], label=algo.label)
+end
+title!("2line-Go, AMPA")
+
+# benchmarks
+bs = Vector{BenchmarkTools.Trial}()
+
+for algo in algorithms
+	push!(bs, @benchmark(
+		evolveSynapse(
+			xc0,
+			xd0,
+			p_synapse,
+			events_sorted_times,
+			is_pre_or_post_event,
+			bap_by_epsp,
+			[true],
+			nu,
+			$(algo).solver,
+			$(algo).agg;
+			save_positions = $(algo).save_positions,
+			saveat = $(algo).saveat,
+			save_everystep = false,
+		),
+		samples = 50,
+		evals = 1,
+		seconds = 500,
+	))
+end
+
+
